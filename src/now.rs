8c3387debc9093f7ecb736237374ae24861619f7
Ben S <ogham@bsago.me>
2015-02-09 21:15:01 +0000

    Rework and add Linux support to now module
    I wondered why the tests were failing on Travis. Changed all the 'use' imports to fully-qualified identifiers, to prevent the compiler complaining about unused imports.

--- src/now.rs
+++ src/now.rs
@@ -1,18 +1,27 @@
 extern crate libc;
 
 #[cfg(any(target_os = "macos", target_os = "ios"))]
-use self::libc::{c_int, timeval, timezone};
-use std::ptr::null_mut;
-
-#[cfg(any(target_os = "macos", target_os = "ios"))]
 extern {
-    fn gettimeofday(tp: *mut timeval, tzp: *mut timezone) -> c_int;
+    fn gettimeofday(tp: *mut libc::timeval, tzp: *mut libc::timezone) -> libc::c_int;
+}
+
+#[cfg(all(unix, not(target_os = "macos"), not(target_os = "ios")))]
+extern {
+    fn clock_gettime(clk_id: libc::c_int, tp: *mut libc::timespec) -> libc::c_int;
 }
 
 /// Return the current time, as a number of seconds and milliseconds.
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 pub unsafe fn now() -> (i64, i16) {
-    let mut tv = timeval { tv_sec: 0, tv_usec: 0 };
-    gettimeofday(&mut tv, null_mut());
+    let mut tv = libc::timeval { tv_sec: 0, tv_usec: 0 };
+    gettimeofday(&mut tv, std::ptr::null_mut());
     (tv.tv_sec as i64, (tv.tv_usec / 1000) as i16)
+}
+
+/// Return the current time, as a number of seconds and milliseconds.
+#[cfg(not(any(target_os = "macos", target_os = "ios", windows)))]
+pub unsafe fn now() -> (i64, i16) {
+    let mut tv = libc::timespec { tv_sec: 0, tv_nsec: 0 };
+    clock_gettime(libc::CLOCK_REALTIME, &mut tv);
+    (tv.tv_sec as i64, (tv.tv_nsec / 1000) as i16)
 }
