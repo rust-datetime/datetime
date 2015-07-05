ae345403526f6742f0ed56a499de593d016814bb
Ben S <ogham@bsago.me>
2015-07-05 21:22:46 +0100

    Add hour and minute fixed offsets, and fix a bug

--- src/zoned.rs
+++ src/zoned.rs
@@ -67,28 +67,66 @@ impl<TZ> TimePiece for ZonedDateTime<TZ> where TZ: TimeZone {
 }
 
 
+/// **Coordinated Universal Time** is the time standard that regulates time
+/// across the world. It does not respect daylight saving time, or undergo any
+/// historical or political changes, which makes it suitable for using as the
+/// 'base time zone' when the actual time zone is not known or relevant.
 #[derive(Debug, Clone)]
 pub struct UTC;
 
 impl TimeZone for UTC {
     fn adjust(&self, local: LocalDateTime) -> LocalDateTime {
-        local
+        local  // No adjustment needed! LocalDateTime uses UTC.
     }
 }
 
 
+/// A time zone with a **fixed offset** differs from UTC by a given number of
+/// seconds. This is usually given in hours, but occasionally minutes are also
+/// specified.
 #[derive(Debug, Clone)]
 pub struct FixedOffset {
     offset: i32,
 }
 
 impl FixedOffset {
+
+    /// Create a new fixed-offset timezone with the given number of seconds.
+    ///
+    /// Panics if the number of seconds is greater than one day's worth of
+    /// seconds (86400) in either direction.
     pub fn of_seconds(seconds: i32) -> FixedOffset {
-        if seconds > -86400 && seconds < 86400 {
-            panic!("Offset greater than one day")
+        if seconds <= -86400 || seconds >= 86400 {
+            panic!("Seconds offset greater than one day ({})", seconds)
         }
         else {
             FixedOffset { offset: seconds }
+        }
+    }
+
+    /// Create a new fixed-offset timezone with the given number of hours and
+    /// minutes.
+    ///
+    /// The values should either be both positive or both negative.
+    ///
+    /// Panics if the numbers are greater than their unit allows (more than 23
+    /// hours or 59 minutes) in either direction, or if the values differ in
+    /// sign (such as a positive number of hours with a negative number of
+    /// minutes).
+    pub fn of_hours_and_minutes(hours: i8, minutes: i8) -> FixedOffset {
+        if hours.signum() != minutes.signum() {
+            panic!("Hour and minute values differ in sign ({} and {}", hours, minutes);
+        }
+        else if hours <= -24 || hours >= 24 {
+            panic!("Hours offset greater than one day ({})", hours);
+        }
+        else if minutes <= -60 || minutes >= 60 {
+            panic!("Minutes offset greater than one hour ({})", minutes);
+        }
+        else {
+            let hours = hours as i32;
+            let minutes = minutes as i32;
+            FixedOffset::of_seconds(hours * 24 + minutes * 60)
         }
     }
 }
@@ -128,4 +166,40 @@ impl TimeZone for VariableOffset {
             Some(t)  => local + Duration::of(t.local_time_type.offset as i64),
         }
     }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::FixedOffset;
+
+	#[test]
+	fn fixed_seconds() {
+		FixedOffset::of_seconds(1234);
+	}
+
+	#[test] #[should_panic]
+	fn fixed_seconds_panic() {
+	    FixedOffset::of_seconds(100_000);
+	}
+
+	#[test]
+	fn fixed_hm() {
+	    FixedOffset::of_hours_and_minutes(5, 30);
+	}
+
+	#[test]
+	fn fixed_hm_negative() {
+	    FixedOffset::of_hours_and_minutes(-3, -45);
+	}
+
+	#[test] #[should_panic]
+	fn fixed_hm_panic() {
+	    FixedOffset::of_hours_and_minutes(8, 60);
+	}
+
+	#[test] #[should_panic]
+	fn fixed_hm_signs() {
+	    FixedOffset::of_hours_and_minutes(-4, 30);
+	}
 }
