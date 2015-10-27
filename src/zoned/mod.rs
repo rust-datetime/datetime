55fb7ccc95a9867a71754025ada5dad88468ba20
Ben S <ogham@bsago.me>
2015-10-27 09:56:52 +0000

    Changes for zoneinfo parsing
    Most of the structures in the zoneinfo crate have now been moved so that they're in the zoneinfo-parse crate. It turns out that the only thing we need to have in common is the transition structure, so that's all that's left!
    
    Also, add basic timezone factory implementation.

--- src/zoned/mod.rs
+++ src/zoned/mod.rs
@@ -1,23 +1,23 @@
-use std::borrow::Cow;
 use std::fmt;
 
+use duration::Duration;
 use instant::Instant;
 use local::LocalDateTime;
 
+pub mod factory;
 mod offset;
 pub mod zoneinfo;
 
 
-pub trait TimeZone<'a>: fmt::Debug {
+pub trait TimeZone: fmt::Debug {
     fn offset(&self, datetime: LocalDateTime) -> i64;
-    fn name(&'a self, datetime: LocalDateTime) -> Cow<'a, str>;
+    fn name(&self, datetime: LocalDateTime) -> &str;
     fn is_fixed(&self) -> bool;
 
     fn from_local(&self, local: LocalDateTime) -> LocalTimes;
 
-    fn to_local(&self, _instant: Instant) -> ZonedDateTime {
-        unimplemented!()
-        // instant + self.offset(instant)
+    fn to_zoned(&self, datetime: LocalDateTime) -> LocalDateTime {
+        datetime + Duration::of(self.offset(datetime))
     }
 }
 
@@ -30,8 +30,8 @@ pub enum LocalTimes {
     Ambiguous { earlier: Instant, later: Instant },
 }
 
-#[derive(Debug)]
-pub struct ZonedDateTime {
-    pub local: LocalDateTime,
-    pub time_zone: Box<TimeZone<'static>>,
-}
+// #[derive(Debug)]
+// pub struct ZonedDateTime {
+//     pub local: LocalDateTime,
+//     pub time_zone: Box<TimeZone<'static>>,
+// }
